a74c5bc2f09132301a5b336277cc40f37723c617
kieuvy611 <nguyenvy13062013@gmail.com>
2026-04-01 13:28:23 +0700

    fix error

--- StafflyApp/StafflyApp/ViewModels/EmployeeViewModel.cs
+++ StafflyApp/StafflyApp/ViewModels/EmployeeViewModel.cs
@@ -1,55 +1,55 @@
-﻿//using System;
-//using System.Collections.ObjectModel;
-//using CommunityToolkit.Mvvm.ComponentModel;
-//using CommunityToolkit.Mvvm.Input;
-//using StafflyApp.Models;
-//using StafflyApp.Data;
+﻿using System;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StafflyApp.Models;
+using StafflyApp.Data;
 
-//namespace StafflyApp.ViewModels
-//{
-//    public partial class EmployeeViewModel : ObservableObject
-//    {
-//        private readonly EmployeeRepository _repository;
-//        [ObservableProperty]
-//        private ObservableCollection<Employee> _employees = new();
+namespace StafflyApp.ViewModels
+{
+    public partial class EmployeeViewModel : ObservableObject
+    {
+        private readonly EmployeeRepository _repository;
+        [ObservableProperty]
+        private ObservableCollection<Employee> employees = new();
 
-//        [ObservableProperty]
-//        private Employee? _selectedEmployee;
-//        private readonly DepartmentRepository _deptRepository = new();
-//        [ObservableProperty]
-//        private ObservableCollection<Department> _departments = new();
-//        public EmployeeViewModel()
-//        {
-//            _repository = new EmployeeRepository();
-//            LoadData();
-//        }
+        [ObservableProperty]
+        private Employee? selectedEmployee;
+        private readonly DepartmentRepository _deptRepository = new();
+        [ObservableProperty]
+        private ObservableCollection<Department> departments = new();
+        public EmployeeViewModel()
+        {
+            _repository = new EmployeeRepository();
+            LoadData();
+        }
 
-//        [RelayCommand]
-//        private void LoadData()
-//        {
-//            var list = _repository.GetAllEmployees();
-//            Employees = new ObservableCollection<Employee>(list);
-//            var deptList = _deptRepository.GetAllDepartments();
-//            Departments = new ObservableCollection<Department>(deptList);
-//        }
+        [RelayCommand]
+        private void LoadData()
+        {
+            var list = _repository.GetAllEmployees();
+            employees = new ObservableCollection<Employee>(list);
+            var deptList = _deptRepository.GetAllDepartments();
+            departments = new ObservableCollection<Department>(deptList);
+        }
 
-//        [RelayCommand]
-//        private void AddNewEmployee()
-//        {
-//            // Logic BE: tạm thời gọi LoadData để refresh
-//            LoadData();
-//        }
+        [RelayCommand]
+        private void AddNewEmployee()
+        {
+            // Logic BE: tạm thời gọi LoadData để refresh
+            LoadData();
+        }
 
-//        [RelayCommand]
-//        private void DeleteSelected()
-//        {
-//            if (SelectedEmployee != null)
-//            {
-//                if (_repository.DeleteEmployee(SelectedEmployee.EmployeeID))
-//                {
-//                    LoadData();
-//                }
-//            }
-//        }
-//    }
-////}
+        [RelayCommand]
+        private void DeleteSelected()
+        {
+            if (selectedEmployee != null)
+            {
+                if (_repository.DeleteEmployee(selectedEmployee.EmployeeID))
+                {
+                    LoadData();
+                }
+            }
+        }
+    }
+}
