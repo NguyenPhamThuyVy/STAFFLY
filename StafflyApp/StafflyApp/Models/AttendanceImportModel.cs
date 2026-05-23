e82367cbde2d81058313a15492ba166f6977ec63
qthink <qthinh1006@gmail.com>
2026-05-23 15:02:31 +0700

    update UI (attendance)

---
+++ StafflyApp/StafflyApp/Models/AttendanceImportModel.cs
@@ -0,0 +1,8 @@
+﻿public class AttendanceImportModel
+{
+    public string DepartmentName { get; set; } // Map với cột Department Name
+    public int AbsenceCount { get; set; }      // Map với cột Absences
+    public int TardyCount { get; set; }        // Map với cột Tardy
+    public bool IsValid { get; set; }          // Để hiện icon check xanh/đỏ
+    public string ErrorMessage { get; set; }   // Để hiện dòng chữ lỗi khi rà chuột vào dòng bị sai
+}
