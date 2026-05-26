e7e3f7b74bd4d2c8ecd42c14841eb2c24fa71e62
Nguyen Pham Thuy Vy <24522059@gm.uit.edu.vn>
2026-05-26 16:36:51 +0700

    fix: Payroll Dashboard

--- StafflyApp/StafflyApp/Views/HRDashboardView.xaml.cs
+++ StafflyApp/StafflyApp/Views/HRDashboardView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Controls;
-using StafflyApp.ViewModels;  
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using StafflyApp.ViewModels;
 
 namespace StafflyApp.Views
 {
@@ -8,7 +10,18 @@ namespace StafflyApp.Views
         public HRDashboardView()
         {
             InitializeComponent();
-            this.DataContext = new DashboardViewModel();
+
+            // Lắng nghe sự kiện mỗi khi trang Dashboard được load/hiển thị lên màn hình
+            this.Loaded += HRDashboardView_Loaded;
+        }
+
+        private void HRDashboardView_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Ép DataContext ép kiểu về đúng ViewModel tổng để ra lệnh làm mới dữ liệu từ Database SQL
+            if (this.DataContext is HRDashboardViewModel viewModel)
+            {
+                viewModel.InitializeOrRefresh();
+            }
         }
     }
 }
